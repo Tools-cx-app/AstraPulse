972556fce23c77f6c05069c4644f17cc8143296c
Hutao <3532340532@qq.com>
2025-02-08 19:38:05 +0800

    更新模块模板

--- modules/service.sh
+++ modules/service.sh
@@ -3,5 +3,24 @@
 # 这将使你的脚本更加兼容，即使Magisk在未来改变了它的挂载点
 MODDIR=${0%/*}
 
-chnod x $MODDIR/LightScheduling
-$MODDIR/LightScheduling >$MODDIR/run.log
+wait_until_login() {
+
+    # in case of /data encryption is disabled
+    while [ "$(getprop sys.boot_completed)" != "1" ]; do
+        sleep 5
+    done
+
+    # we doesn't have the permission to rw "/sdcard" before the user unlocks the screen
+    # shellcheck disable=SC2039
+    local test_file="/sdcard/Android/.PERMISSION_TEST_FREEZEIT"
+    true >"$test_file"
+    while [ ! -f "$test_file" ]; do
+        sleep 5
+        true >"$test_file"
+    done
+    rm "$test_file"
+}
+
+wait_until_login
+
+$MODDIR/LightScheduling > $MODDIR/run.log
