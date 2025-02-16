4d10e044fffb14118819ca4b267ff21ea10b56a3
Hutao <3532340532@qq.com>
2025-02-16 19:00:51 +0800

    更新模板

--- modules/customize.sh
+++ modules/customize.sh
@@ -1,2 +1,7 @@
 # 注意 这不是占位符！！这个代码的作用是将模块里的东西全部塞系统里，然后挂上默认权限
-SKIPUNZIP=0
+
+ui_print "专为萌新做的调度"
+ui_print "配置文件在/data/adb/modules/AstraPulse/config.toml"
+ui_print "/data/adb/modules/AstraPulse/config/目录下的文件是自定义频率等文件，文件必须以toml结尾"
+set_perm_recursive $MODPATH 0 0 0755 0644
+set_perm $MODPATH/AstraPulse 0 0 0755
